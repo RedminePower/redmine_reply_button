904f0d45662555471f0b6d59ff56ea6417313451
SabastianGambrell <sabastian.gambrell@gmail.com>
2022-10-10 15:16:58 +0900

    Bug Fixed

--- init.rb
+++ init.rb
@@ -7,7 +7,8 @@ Rails.logger.info 'Starting Redmine Reply Button'
 Redmine::Plugin.register :redmine_reply_button do
     name 'Redmine Reply Button'
     author 'SabastianGambrell'
-    description 'Adds "Reply" button to the action menu of the issue page.'
+    description 'Add "Reply" button to the action menu of the issue page.'
+    url 'https://github.com/SabastianGambrell/redmine_reply_button'
     version '1.0.0'
     
     project_module :issue_reply_button do
