5dc3a0124ac29dd04d4d1ce1b4a6e03858a31d9e
RedminePower <redmine.power.com@gmail.com>
2025-01-09 17:59:42 +0900

    update author and url

--- init.rb
+++ init.rb
@@ -4,9 +4,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_reply_button do
     name 'Redmine Reply Button'
-    author 'SabastianGambrell'
+    author 'Redmine Power'
     description 'Add "Reply" button to the action menu of the issue page.'
-    url 'https://github.com/SabastianGambrell/redmine_reply_button'
+    url 'https://github.com/RedminePower/redmine_reply_button'
     version '1.2.1'
     
     project_module :reply_button do
