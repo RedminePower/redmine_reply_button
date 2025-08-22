bbb0ebe19890d667825de633ba054b76cdfddced
RedminePower <redmine.power.com@gmail.com>
2025-08-22 12:13:41 +0900

    Fix bug when assign to group

--- init.rb
+++ init.rb
@@ -7,7 +7,8 @@ Redmine::Plugin.register :redmine_reply_button do
     author 'Redmine Power'
     description 'Add "Reply" button to the action menu of the issue page.'
     url 'https://github.com/RedminePower/redmine_reply_button'
-    version '1.2.1'
+    author_url 'https://www.redmine-power.com/'
+    version '1.2.2'
     
     project_module :reply_button do
         permission :reply_button, :reply_button => [:index]
